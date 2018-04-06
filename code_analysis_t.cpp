924a51dc918a9d988d7527e18c3be103451a0146
Ryan <rjc84@zips.uakron.edu>
2018-04-06 15:24:37 -0400

    Created test for Filename

--- code_analysis_t.cpp
+++ code_analysis_t.cpp
@@ -6,6 +6,7 @@
 
 #include "code_analysis.hpp"
 
+#include <iostream>
 #include <string>
 #include <cassert>
 
@@ -21,11 +22,20 @@ int main() {
         request.option_url      = "";
         request.option_language = "";
 
-        auto filename = analysis_filename(request);
+	auto filename = analysis_filename(request);
         assert(filename == "");
         assert(analysis_url(request) == "");
         assert(analysis_language(request, filename) == "");
         assert(code_analysis(request) == false);
+
+	//Tests
+	std::cout << "From the code_analysis function: " << std::endl;
+	std::cout << "Good name: " << std::endl;
+	request.option_filename = "test.cpp";
+	assert(code_analysis(request) == false);
+	std::cout << std::endl;
+
+	
     }
 
     return 0;
