7cdf093cf61f331aa76a61b8bd631dfb172a00b9
Ryan <rjc84@zips.uakron.edu>
2018-04-06 16:26:23 -0400

    added filename, language and url tests

--- code_analysis_t.cpp
+++ code_analysis_t.cpp
@@ -31,16 +31,44 @@ int main() {
 	//Tests
 	std::cout << "From the code_analysis function: " << std::endl;
 	std::cout << "Good name: " << std::endl;
+	std::cout << "test.cpp" << std::endl;
 	request.option_filename = "test.cpp";
 	assert(code_analysis(request) == false);
 	std::cout << std::endl;
 
 	std::cout << "Bad name: " << std::endl;
+	std::cout << "test.wrong" << std::endl;
 	request.option_filename = "test.wrong";
 	assert(code_analysis(request) == false);
 	std::cout << std::endl;
-	
 
+	std::cout << "filename test" << std::endl;
+
+	request.option_filename = "";
+	request.entry_filename = "data";
+	request.given_filename = "test.cpp";
+	std::cout << analysis_filename(request) << std::endl;
+
+	std::cout << "language test" << std::endl;
+
+	std::cout << "Good language: " << std::endl;
+	request.option_language = "C++";
+	std::cout << analysis_language(request, analysis_filename(request)) << std::endl;
+	std::cout << "Bad language: " << std::endl;
+	request.option_language = "";
+	std::cout << analysis_language(request, analysis_filename(request)) << std::endl;
+
+	std::cout << "url test" << std::endl;
+
+	request.given_url = "https://google.com";
+	request.option_url = "www.test.com";
+	std::cout << "Good url: " << std::endl;
+	std::cout<< analysis_url(request) << std::endl;
+	std::cout << "bad url: " << std::endl;
+	request.option_url = "";
+	std::cout << analysis_url(request) << std::endl;
+	
+	
 	
     }
 
